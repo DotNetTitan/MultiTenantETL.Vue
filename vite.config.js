d056d1d3a1120fe1771aad6ecdf50e04e759bf00
DotnetTitan <dotnettitan@gmail.com>
2025-04-04 10:04:23 +0530

    Add production environment configuration and update Vite setup for module resolution

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/MultiTenantETL.Vue/',
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
-  base: '/MultiTenantETL.Vue/'
+  server: {
+    port: 5173
+  }
 })
