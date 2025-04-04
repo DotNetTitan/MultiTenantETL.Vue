be354b015a29ed8fe64861b5667ed00dae75ca4c
DotnetTitan <dotnettitan@gmail.com>
2025-04-04 09:51:13 +0530

    Refactor Vite configuration to use path.resolve for alias and restore base path

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/MultiTenantETL.Vue/',  // Add this line
   plugins: [
     vue(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
   },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'https://localhost:7103',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
+  base: '/MultiTenantETL.Vue/'
 })
