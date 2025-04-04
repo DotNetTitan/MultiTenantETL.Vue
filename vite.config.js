71157a884760024f5f7077e460fa90befa3936b9
DotnetTitan <dotnettitan@gmail.com>
2025-04-04 09:42:40 +0530

    Update GitHub Actions workflow for Vue app deployment and configure base path in Vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/MultiTenantETL.Vue/',  // Add this line
   plugins: [
     vue(),
   ],
